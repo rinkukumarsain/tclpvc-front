752cb6a331da5120b531daf2e4dae299241b6b14
unknown <rksainimg@gmail.com>
2021-02-18 11:17:18 +0530

    first react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Tclpvcddsfd</h1>
+     <button >Ritik</button>
+     <Button>Rinku</Button>
+    </div>
+  );
+}
+
+export default App;
